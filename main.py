9a34e11eea805b7baaa66a1d0187600e32540c56
Henrique Barbosa <henrikleod@gmail.com>
2022-01-29 15:54:56 -0300

    Implementing kuduro translator on api routes

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI
 from models.Translation import Translation
-from lib.kuduairo_translator.entrypoint import Entrypoint
+from lib.kuduairo_translator.entrypoint import Entrypoint as Translator
 import uvicorn
 
 app = FastAPI()
+translator = Translator()
+
+@app.get("/")
+def index():
+    return { 'status': 'OK' }
 
 @app.post("/pt-to-kd/")
 def brazilian_portuguese_to_kuduairo(translation: Translation):
-    kuduairo_phonemes = [
-        "uro"
-    ]
-        
-    brazilian_portuguese_phonemes = [
-        "uairo"
-    ]
-    current_sentence = translation.sentence
-
-
-    return {'message': current_sentence.replace('uro', 'uairo')}
+    return {
+        'status': 'success',
+        'current_language': translation.target_language,
+        'original_language': translation.current_language,
+        'translation': translator.pt_to_kd(translation.sentence)
+    }
 
 @app.post("/kd-to-pt/")                             
 def kuduairo_to_brazilian_portuguese(translation: Translation):
