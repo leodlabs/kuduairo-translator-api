83880ad40dd4c60fe66dc52a4aece5434ddee51b
Henrique Barbosa <henrikleod@gmail.com>
2022-02-01 20:56:56 -0300

    CORS stuff

--- server/api.py
+++ server/api.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from .routes import router as TranslateRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://kuduairo.netlify.app",
+    "http://192.168.1.70:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["Root Path"])
 async def index():
